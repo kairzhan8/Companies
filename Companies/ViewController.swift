db9c39cf3025e3cd43e9af2b18f87cef0afd914f
kairzhan8 <kairzhankural@gmail.com>
2020-10-03 13:28:20 +0600

    Initial Commit

---
+++ Companies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Companies
+//
+//  Created by Kairzhan Kural on 10/3/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
