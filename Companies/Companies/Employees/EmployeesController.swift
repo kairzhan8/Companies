99a9e8c8c59372bc6d5546e2ba3649ed865ce21d
kairzhan8 <kairzhankural@gmail.com>
2020-10-14 21:03:53 +0600

    work with sections

--- Companies/Companies/Employees/EmployeesController.swift
+++ Companies/Companies/Employees/EmployeesController.swift
@@ -9,6 +9,14 @@
 import UIKit
 import CoreData
 
+class IndendedLabel: UILabel {
+    override func drawText(in rect: CGRect) {
+        let padding = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        let rectangle = bounds.inset(by: padding)
+        super.drawText(in: rectangle)
+    }
+}
+
 class EmployeesController: UITableViewController, CreateEmployeeControllerDelegate {
     
     func didAddEmployee(employee: Employee) {
@@ -20,6 +28,11 @@ class EmployeesController: UITableViewController, CreateEmployeeControllerDelega
     var company: Company?
     var employees: [Employee] = []
     
+    var shortNameEmployees: [Employee] = []
+    var longNameEmployees = [Employee]()
+    var reallyLongNameEmployees = [Employee]()
+    var allEmployees = [[Employee]]()
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -29,7 +42,28 @@ class EmployeesController: UITableViewController, CreateEmployeeControllerDelega
     private func fetchEmployees() {
         print("Trying to fetch employees from core data")
         guard let employees = company?.employees?.allObjects as? [Employee] else { return }
-        self.employees = employees
+        shortNameEmployees = employees.filter({ (employee) -> Bool in
+            if let count = employee.name?.count {
+                return count < 5
+            }
+            return false
+        })
+        longNameEmployees = employees.filter({ (employee) -> Bool in
+            if let count = employee.name?.count {
+                return count <= 8 && count >= 5
+            }
+            return false
+        })
+        
+        reallyLongNameEmployees = employees.filter({ (employee) -> Bool in
+            if let count = employee.name?.count {
+                return count > 8
+            }
+            return false
+        })
+        
+        allEmployees = [shortNameEmployees, longNameEmployees, reallyLongNameEmployees]
+//        self.employees = employees
 //        let context = CoreDataManager.shared.persistantContainer.viewContext
 //
 //        let fetch = NSFetchRequest<Employee>(entityName: "Employee")
@@ -57,15 +91,37 @@ class EmployeesController: UITableViewController, CreateEmployeeControllerDelega
 
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        allEmployees.count
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = IndendedLabel()
+        label.backgroundColor = .lightBlue
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = .darkBlue
+        if section == 0 {
+            label.text = "Short name"
+        } else if section == 1 {
+            label.text = "Long name"
+        } else {
+            label.text = "Really long name"
+        }
+        return label
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        50
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        employees.count
+        allEmployees[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        
-        let employee = employees[indexPath.row]
+         
+        let employee = allEmployees[indexPath.section][indexPath.row]
         cell.textLabel?.text = employee.name
         if let birthday = employee.employeeInformation?.birthday {
             let dateFormatter = DateFormatter()
